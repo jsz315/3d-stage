66e0c446bb9eb186e4417f1211cb70c069b031a8
jsz315 <jsz_thing@126.com>
2019-09-02 17:51:54 +0800

    添加指令

--- src/components/GeometryView/index.vue
+++ src/components/GeometryView/index.vue
@@ -4,7 +4,7 @@
 
       <div class="comm" v-if="info.position">
         <div class="label" @click="log(info.position)">position</div>
-        <div class="info">
+        <div class="info" @click="log(info.position, true)">
           <div class="tip">length: {{info.position.count * info.position.itemSize}}</div>
           <div class="tip">count: {{info.position.count}}</div>
           <div class="tip">itemSize: {{info.position.itemSize}}</div>
@@ -13,7 +13,7 @@
 
       <div class="comm" v-if="info.normal">
         <div class="label" @click="log(info.normal)">normal</div>
-        <div class="info">
+        <div class="info" @click="log(info.normal, true)">
           <div class="tip">length: {{info.normal.count * info.normal.itemSize}}</div>
           <div class="tip">count: {{info.normal.count}}</div>
           <div class="tip">itemSize: {{info.normal.itemSize}}</div>
@@ -22,7 +22,7 @@
 
       <div class="comm" v-if="info.uv">
         <div class="label" @click="log(info.uv)">uv</div>
-        <div class="info">
+        <div class="info" @click="log(info.uv, true)">
           <div class="tip">length: {{info.uv.count * info.uv.itemSize}}</div>
           <div class="tip">count: {{info.uv.count}}</div>
           <div class="tip">itemSize: {{info.uv.itemSize}}</div>
@@ -31,7 +31,7 @@
 
       <div class="comm" v-if="info.index">
         <div class="label" @click="log(info.index)">index</div>
-        <div class="info">
+        <div class="info" @click="log(info.index, true)">
           <div class="tip">length: {{info.index.count}}</div>
         </div>
       </div>
@@ -59,8 +59,14 @@
       }
     },
     methods: {
-      log(n) {
-        console.log(n.array);
+      log(n, join) {
+        if(join){
+          console.log("[" + n.array.join(",") + "]");
+        }
+        else{
+          console.log(n.array);
+        }
+        
       }
     }
   };

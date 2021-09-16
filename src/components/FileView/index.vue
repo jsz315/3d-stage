44008ae69968827a9ff08fa209b83b98bcc56615
jsz315 <jsz_thing@126.com>
2021-09-16 22:44:59 +0800

    添加loadfile

--- src/components/FileView/index.vue
+++ src/components/FileView/index.vue
@@ -1,6 +1,6 @@
 <template>
     <PopView :visible="visible.file" title="模型" :close="onHide" width="80%">
-        <div class="box">
+        <div class="box" :class="{ hited }" ref="box">
             <div class="tip">拖动文件到框内</div>
             <input class="file" type="file" @change="onChange" />
         </div>
@@ -12,17 +12,60 @@ import GameEvent from "@/core/event/index";
 import PopView from "@/components/PopView/index.vue";
 import { mapState, mapMutations } from "vuex";
 import axios from "axios";
+import { H5EventDispatcher } from "jsz-event";
+
+let dropRemove, dragoverRemove, dragleaveRemove;
 export default {
     data() {
         return {
-            list: []
+            list: [],
+            hited: false
         };
     },
     components: { PopView },
     computed: {
         ...mapState(["visible"])
     },
-    mounted() {},
+    mounted() {
+        const dragWrapper = this.$refs.box;
+        dropRemove = H5EventDispatcher.createDomListenter(
+            dragWrapper,
+            "drop",
+            e => {
+                this.hited = false;
+                e.preventDefault(); //阻止e的默认行为
+                var files = e.dataTransfer.files;
+                if (files && files[0]) {
+                    GameEvent.ins.send(GameEvent.IMPORT_FILE, files[0]);
+                }
+                this.onHide();
+            }
+        );
+
+        dragoverRemove = H5EventDispatcher.createDomListenter(
+            dragWrapper,
+            "dragover",
+            e => {
+                e.preventDefault();
+                console.log("dragover");
+                this.hited = true;
+            }
+        );
+
+        dragleaveRemove = H5EventDispatcher.createDomListenter(
+            dragWrapper,
+            "dragleave",
+            e => {
+                console.log("dragleave");
+                this.hited = false;
+            }
+        );
+    },
+    beforeDestory() {
+        dropRemove && dropRemove();
+        dragoverRemove && dragoverRemove();
+        dragleaveRemove && dragleaveRemove();
+    },
     methods: {
         ...mapMutations(["changeVisible"]),
         onHide() {
